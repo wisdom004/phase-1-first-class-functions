9c3fcb40fc3d8ab2f5819d128e6bd8d0eb99b61b
Ian <ian.njoroge2@student.moringaschool.com>
2025-03-06 21:57:40 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("I am a named function!");
+    }
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I am an anonymous function!");
+    };
+}
